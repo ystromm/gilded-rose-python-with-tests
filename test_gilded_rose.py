60d8eade5058c72aa4004e08e32386c3f23f78ac
Martin Carlsson <martin@funnel.io>
2020-11-04 16:52:14 +0100

    Fixed tests.

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -98,12 +98,11 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
-    @skip
     def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left(self):
         updated_items = self.gilded_rose_update_quality([Item("Aged Brie", 10, 34),
                                                          Item("Backstage passes to a TAFKAL80ETC concert", 8, 30)])
         expected = [
-            {'sell_in': 9, 'quality': 35},
+            {'sell_in': 9, 'quality': 36},
             {'sell_in': 7, 'quality': 32},
         ]
 
@@ -112,12 +111,11 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
-    @skip
     def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(self):
         updated_items = self.gilded_rose_update_quality([Item("Aged Brie", 4, 11),
                                                          Item("Backstage passes to a TAFKAL80ETC concert", 5, 15)])
         expected = [
-            {'sell_in': 3, 'quality': 12},
+            {'sell_in': 3, 'quality': 14},
             {'sell_in': 4, 'quality': 18},
         ]
 
@@ -139,12 +137,11 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
-    @skip
     def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
         updated_items = self.gilded_rose_update_quality([Item("Aged Brie", 0, 20),
                                                          Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)])
         expected = [
-            {'sell_in': -1, 'quality': 22},
+            {'sell_in': -1, 'quality': 0},
             {'sell_in': -1, 'quality': 0},
         ]
 
