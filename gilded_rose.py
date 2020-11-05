af12b5b7ccf83555154ee4fb358045fb024327b2
Johanna Samuelsen <johanna@funnel.io>
2020-11-05 15:40:43 +0100

    backstage passes

--- gilded_rose.py
+++ gilded_rose.py
@@ -13,37 +13,41 @@ class GildedRose:
                 pass
             elif AGED_BRIE == item.name:
                 GildedRose.update_aged_brie(item)
+            elif BACKSTAGE_PASSES == item.name:
+                GildedRose.update_backstage_passes(item)
             else:
                 GildedRose.update_item(item)
         return items
 
     @staticmethod
     def update_item(item):
-        if BACKSTAGE_PASSES != item.name:
-            # TODO: Improve this code.  Word.
-            if item.quality > 0:
-                item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                if BACKSTAGE_PASSES == item.name:
-                    if item.sell_in < 11:
-                        # See revision number 2394 on SVN.
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+        # TODO: Improve this code.  Word.
+        if item.quality > 0:
+            item.quality = item.quality - 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            if BACKSTAGE_PASSES != item.name:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-            else:
-                # TODO: Fix this.
-                item.quality = item.quality - item.quality
+            if item.quality > 0:
+                item.quality = item.quality - 1
+        if item.quality > 50:
+            item.quality = 50
+
+    @staticmethod
+    def update_backstage_passes(item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            # Increases the Quality of the stinky cheese if it's 11 days to due date.
+            if item.sell_in < 11:
+                # See revision number 2394 on SVN.
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            # Increases the Quality of Backstage Passes if the Quality is 6 or less.
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            # TODO: Fix this.
+            item.quality = item.quality - item.quality
         if item.quality > 50:
             item.quality = 50
 
