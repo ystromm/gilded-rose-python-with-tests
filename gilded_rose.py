1b25eaf296c2768941191d8803600343307eca29
Johanna Samuelsen <johanna@funnel.io>
2020-11-05 15:32:48 +0100

    delete aged brie

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,26 +11,22 @@ class GildedRose:
                 #do something with sulfuris
                 item.quality = 80
                 pass
+            elif AGED_BRIE == item.name:
+                GildedRose.update_aged_brie(item)
             else:
                 GildedRose.update_item(item)
         return items
 
     @staticmethod
     def update_item(item):
-        if AGED_BRIE != item.name and BACKSTAGE_PASSES != item.name:
+        if BACKSTAGE_PASSES != item.name:
             # TODO: Improve this code.  Word.
             if item.quality > 0:
                 item.quality = item.quality - 1
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
-                if AGED_BRIE == item.name:
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
                 # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                if AGED_BRIE == item.name:
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
                 if BACKSTAGE_PASSES == item.name:
                     if item.sell_in < 11:
                         # See revision number 2394 on SVN.
@@ -42,18 +38,29 @@ class GildedRose:
                             item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            if AGED_BRIE != item.name:
-                if BACKSTAGE_PASSES != item.name:
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    # TODO: Fix this.
-                    item.quality = item.quality - item.quality
+            if BACKSTAGE_PASSES != item.name:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                if AGED_BRIE == item.name and item.sell_in <= 0:
-                    item.quality = 0
-                    # of for.
+                # TODO: Fix this.
+                item.quality = item.quality - item.quality
+        if item.quality > 50:
+            item.quality = 50
+
+
+    def update_aged_brie(item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 6:
+                item.quality = item.quality + 1
+            # Increases the Quality of the stinky cheese if it's 11 days to due date.
+            if item.sell_in < 11:
+                item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+            if item.sell_in <= 0:
+                item.quality = 0
         if item.quality > 50:
             item.quality = 50
